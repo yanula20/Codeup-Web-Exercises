a2889458b3992e330dd43f14478c4984e33b8062
Donald Moore <donmoore26762671@gmail.com>
2016-01-22 10:35:39 -0600

    Completed homework exercise on Google API.

--- public/my_first_form.php
+++ public/my_first_form.php
@@ -1,76 +1,61 @@
 <!DOCTYPE html>
-<!--HTML inputs should also have a name attribute that describes what the 
-    input is. The name attribute for the input will appear as the key in the 
-    query string when the form is submitted.-->  
-
-    <!--Form inputs can be defined by using the HTML <input> element. The 'input' 
-    element is a void element, since it doesn't have any inner content.-->  
-
-    <!--Additionally, an id attribute with the same value as the name attribute is 
-    often specified with the 'input'. The id attribute is used for input labeling 
-    and also for easy access via JavaScript. --> 
-
-	<!--Some input types, like submit, text, placeholder, password, and email, can 
-	have an initial value set by using the value attribute.-->
-
-	<!--'<button type="submit">Login</button>'.This example will display the same result 
-	as the previous one. The main reason to use a 'button> over an <input type="submit> 
-	is that the <button> element is not a void element and can contain other elements. 
-	t also offers more styling possibilities.-->
-
-	<!--margin-right works well for expanding labels across the page.-->
-
-	<!--Do not leave out the "name" attribute in your input fields, test choices,
-	blanks of fill-in forms. Without them, key (name)=value (user-defined) string will not be submitted to the
-    server. You don't need a "name" for Submit, Enter buttons-->
-		
 
 <html>
 <head>
 	<meta charset="utf-8">
 
-	<?php var_dump($_GET); 
-	var_dump($_POST);
+	<?php 
+		var_dump($_GET); 
+		var_dump($_POST);
 	?>
 
-    <title>My First HTML form</title>
-    
+    <title>My First HTML Form</title>
     <style type="text/css">
 
-    body h4 { 
-    	background-color:
-    	rgba(255, 0, 0, 0.3);  /* red with opacity */
-    	width: 90%;
-    	text-decoration: underline; 
-    	margin-bottom: 10px;
-    }
+	    body h4 { 
+	    	background-color:
+	    	rgba(255, 0, 0, 0.3);  /* red with opacity */
+	    	width: 80%;
+	    	text-decoration: underline; 
+	    	margin-bottom: 10px;
+	    }
 
-    body label { background-color: yellow; margin-right: 20px
+	    body label { 
+	    	background-color: yellow; 
+	    	margin-right: 20px
+	    }
 
-    }
+	    body option { 
+	    	background-color: orange; width: 50%; 
+	    	margin-right: 20px;
+	    	/* Within labels and options, experiment with background color, width %, 
+		margin-right px */
+	    }
+	    
+	    body { 
+	    	font-family: "comic sans ms", Times, "Papyrus"; 
+	    }
 
-    body option { background-color: orange; width: 50%; margin-right: 20px;
-    	/* Within labels and options, experiment with background color, width %, 
-	margin-right px */
-    }
-    
-    body { 
-    	font-family: "comic sans ms", Times, "Papyrus" 
-    }
+	    body button { 
+	    	background-color: violet; 
+	    	margin-left: 0px;
+	    }
+	    
+	    #Compose_e-mail textarea { 
+	    	color: red;
+	    }
 
-    body button { 
-    	background-color: violet; 
-    	font-family: "comic sans ms", Times, "Papyrus" 
-    }
-    #Compose_e-mail textarea { color: red;
-    }
+	 	#Compose_e-mail label { 
+	 		color: blue;
+	    }
+	     
+	    #Compose_e-mail button { 
+	    	color: red;
+	    }
 
- 	#Compose_e-mail label { color: blue;
-    }
-     
-    #Compose_e-mail button { color: red;
-    }
-
+	    #usrPwd {
+	    	background-color: violet;
+	    }
     </style>
     
 </head>
@@ -80,7 +65,7 @@
 	    <p>
 	        <label for="username">Username</label>
 	        <input id="username" name="username" placeholder="Enter your username" 
-	        type="text">
+	        type="text"> <!-- test with a value = user1 in input -->
 	    </p>
 	    <p>
 	        <label for="password">Password</label>
@@ -88,11 +73,13 @@
 	        type="password">
 	    </p>
 	    <p>
-	    	<label for="Login"></label>
-	        <button id="Login" name="Login" type="Submit"><strong>Login</strong></button>
+	    	<button type="Submit">Login</button>
+	        <input type="Submit" id="usrPwd">
 	    </p>
 	</form>
-	
+
+	<hr>
+
 	<form>
 		<h4 id="Compose_e-mail">Compose an e-mail</h4>
 
@@ -112,7 +99,10 @@
 		<button id="Sent_folder" name="sent_folder" type="Submit" checked><strong>Send</strong></button>
 	</form> 
 
+	<hr>
+
 	<form>
+
 	<!--Giving radio buttons the same name puts them in the same group. 
 	Only one radio button in a group can be checked at a time. 
 	The name for radio buttons does not need to end with square brackets [] 
@@ -145,17 +135,24 @@
 	    <button id="Multiple_Choice_Test" type="Submit"><strong>Submit Answers</strong></button>
 	</form>
 
+	<hr>
+
 	<form>
 	<h4>Select Testing-Who is the current US President?</h4>
 		<label for="President">Select the correct answer from the drop-down menu:></label><br>
 		<select id="President" name="President">
 			<option>Barack Obama</option>
 			<option selected>George Washington</option>
+			<option>The Great Tom Izzo</option>
+			<option>Peter Falk</option>
+			<!-- defaulted to Barry O -->
+		</select>
+	</br>
 		<label for="Presidents_Test"></label>
 	    <button id="Presidents_Test" name="Presidents_Answers" type="Submit"><strong>Submit Answers</strong></button>
-
-		</select>
 	</form>
+
+	<hr>
 
 	<form>
 	<h4>Select Testing-Which cities have NFL football teams?</h4>
@@ -168,41 +165,54 @@
 			<option name="city5" value="Annapolis, MD">Annapolis, MD</option>
 			<option name="city6" value="Monterey, CA">Monterey, CA</option>
 			<option name="city7" value="Hattiesburg, MS">Hattiesburg, MS</option>
-
+		</select>
+	</br>
 		<label for="Football_Test"></label>
 	    <button id="Football_Test" name="Football_Answers" type="Submit"><strong>Submit Answers</strong></button>
-		</select>
-	</form>
+		</form>
+	<hr>
 
 	<form>
 	<h4>Select Testing-Which of the following are American-made cars?:</h4>
 		<label for="American_car">Select the correct answers below:</label><br>
-		<select id="American_Car" name="car[]" multiple>
+		<select id="American_Car" name="car">
 			<option value="Ford Escort">Ford Escort</option>
 			<option value="Chrysler LeBaron">Chrysler LeBaron</option>
 			<option value="Yugo">Yugo</option>
 			<option value="Buick Enclave">Buick Enclave</option>
 			<option value="Nissan Pathfinder">Nissan Pathfinder</option>
 			<option value="Audi">Audi</option>
-			<option value="Ford Focus">Ford Focus</option>			
+			<option value="Ford Focus">Ford Focus</option>
+			<!-- take away multiple and array [] box, multiple deleted makes a 
+			small box with the first option selected by default	 -->
+
+			<!-- Note: selected can be added to any option as well as the first 
+			one in the option list<option value="Buick Enclave" selected> -->
+
+			<!-- <option selected disabled>Please Chose</option>	 -->
 		</select><br>
 		<button id="USA_cars" type="text" type="Submit"><strong>Send</strong></button>
 	</form>
+
+	<hr>
 
 	<form>
 	<h4>Select Testing-Which of the following are American-made cars?:</h4>
 		<label for="American_car">Select the correct answers below:</label><br>
 		<select id="American_Car" name="car[]" multiple>
-			<option>Ford Escort</option>
-			<option>Chrysler LeBaron</option>
-			<option>Yugo</option>
-			<option>Buick Enclave</option>
-			<option>Nissan Pathfinder</option>
-			<option>Audi</option>
-			<option>Ford Focus</option>
+			<option value="1">Ford Escort</option>
+			<option value="2">Chrysler LeBaron</option>
+			<option value="3">Yugo</option>
+			<option value="4">Buick Enclave</option>
+			<option value="5">Nissan Pathfinder</option>
+			<option value="6">Audi</option>
+			<option value="7">Ford Focus</option>
+			<!-- must hold down the command key to select multiple answers -->
 		</select><br>
 		<button id="USA_cars2" type="text" type="Submit"><strong>Send</strong></button>
 	</form>
+
+	<hr>
 
 	<form>
 	<h4>Select Testing - Operating Systems</h4>
