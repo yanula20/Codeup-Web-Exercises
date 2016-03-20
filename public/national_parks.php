5d0629814807f63c2a83392371379bd86149ca9b
Donald Moore <donmoore26762671@gmail.com>
2016-03-20 07:34:44 -0500

    Google maps API was fixed with restaurants was fixed. Do not put html tags in the address obj. Worked on styling the National Parks PHP,html,css, mysql, pagination exercise. Worked on creating radius circles for Research Project - mashing GoogleAPI, Github.

--- public/national_parks.php
+++ public/national_parks.php
@@ -8,20 +8,16 @@ $page = Input::has('page')? Input::get('page') : 1;
 
 $limit = 4;
 
-
 $offset = ($limit * $page)-$limit;
 
 $stmt = $dbc->query("SELECT * FROM national_parks LIMIT $limit OFFSET $offset");
  
 $parks = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-
 //php either back ticks or nothing, pdostate obj -> with fetchColumn string with num, typecasted below
 $count = $dbc->query("SELECT count('id') FROM national_parks")->fetchColumn();
 
 $total_pages = $count/4;
-
-
 
 ?>
 <!DOCTYPE html>
@@ -29,45 +25,42 @@ $total_pages = $count/4;
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Template</title>
+    <title>National Parks Exercise</title>
     <!-- order matters, my own stylesheet must go unederneath -->
 
     <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
 
     <link href='https://fonts.googleapis.com/css?family=Roboto+Slab' rel='stylesheet' type='text/css'> 
     <!---external personalized stylesheet-->
-    <style type="text/css">
 
-    /*css here*/
+    <link rel="stylesheet" type="text/css" href="/css/parks_php_mysql.css">
 
-    </style>  
 </head>
 <body>
     <div id="wrapper">
 
+    <h1>National Parks</h1>
     <?php foreach($parks as $park): ?>
-    <h2><?=$park['name'] ?><h2>
-    <h3><?=$park['location'] ?><h3>
-    <h3><?=$park['date_established'] ?><h3>
-    <h3><?=$park['area_in_acres'] ?><h3>
+    <h2>Park name: <?=$park['name'] ?><h2>
+    <h3>location: <?=$park['location'] ?><h3>
+    <h4>Date established: <?=$park['date_established'] ?><h4>
+    <h4>acres: <?=$park['area_in_acres'] ?><h4>
     <?php endforeach; ?>
      
     <?php if($page < $total_pages) { ?> 
     <a href="national_parks.php?page=<?=($page+1)?>">NEXT</a><br>
     <?php } ?>
   
-    <?php if($page > 1 ) { ?>     
+    <?php if($page > 1) { ?>     
     <a href="national_parks.php?page=<?=($page-1)?>">PREVIOUS</a><br>
     <?php } ?>
      <!--html here-->
-
 
     </div> <!-- end wrapper   --> 
 
 <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
 <script src="/js/bootstrap.js"></script>
 
-<script src="/jquery-ui-1.11.4.custom/js/query-ui.min.css"></script>
 <script type="text/javascript">
       /*--js here--*/
 </script>
